89b94fbe9a668973a2f98a3f7c59251ec50a08d9
fimoefive <brothermj@hotmail.com>
2020-10-01 13:36:44 -0500

    .find array functions in findExercises.js.

--- src/findExercises.js
+++ src/findExercises.js
@@ -7,31 +7,67 @@ const instructors = useInstructors();
 // It should accept one integer parameter named `id`
 // It should return the student object with the matching ID
 // Ex: getStudentById(1)
+export const getStudentById = (id) => {
+    const foundId = students.find((studentId) => {
+    return studentId.id === id;
+    })
+    return foundId;
+};
+// console.log(students.find(getStudentById));
 
 // Export a function called getInstructorById
 // It should accept one integer parameter named `id`
 // It should return the instructor object with the matching ID
 // Ex: getInstructorById(1)
+export const getInstructorById = (id) => {
+    const foundInstructor = instructors.find((instructorId) => {
+    return instructorId.id === id;
+    })
+    return foundInstructor;
+};
 
 // Export a function called getStudentByLastName
 // It should accept one string parameter named `lastName`
 // It should return the student object whose last name matches `lastName`
 // It should NOT be case sensitive
 // Ex: getStudentByName("sMiTh")
+export const getStudentByLastName = (lastName) => {
+    const foundName = students.find((studentName) => {
+        return studentName.lastName === lastName;
+    })
+    return foundName;
+};
 
 // Export a function called getStudentByName
 // It should accept one string parameter named `fullName`
 // It should return the student object whose first and last name match `fullName`
 // It should NOT be case sensitive
 // Ex: getStudentByName("Summer SMITH")
+export const getStudentByName = (fullName) => {
+    const findStudent = students.find((student) => {
+        const fullName = student.firstName + " " + student.lastName
+        return fullName === fullName;
+    })
+    return findStudent;
+};
 
 // Export a function called getInstructorOfStudent
 // It should accept one integeter parameter named `studentId`
 // It should return the instructor object of the student whose id matches `studentId`
 // Ex: getInstructorOfStudent(4)      // returns Brenda Long
+export const getInstructorOfStudent = (studentId) => {
+    const instuctorOfStudent = instructors.find((instructor) => {
+        return instuctorOfStudent.studentId === studentId:
+    })
+    return instuctorOfStudent;
+};
 
 // Export a function called getStudentWithMostLangs
 // It should not accept any parameters
 // It should return the student object who knows the most programming languages
 // Ex: getStudentWithMostLangs()      // returns Rick Sanchez
 // HINT: You may not need the `find` method for this. This is one of the few cases where a `for` loop might be appropriate
+export const getStudentWithMostLangs = () => {
+
+    return
+};
